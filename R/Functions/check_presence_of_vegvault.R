20667864b607c68108d7f46041827639f6a1c325
Ondřej Mottl <ondrej.mottl@gmail.com>
2025-04-18 14:35:13 +0200

    added utiliy functions to check the presence of VegVault and extract config values

--- R/Functions/check_presence_of_vegvault.R
+++ R/Functions/check_presence_of_vegvault.R
@@ -1,9 +1,7 @@
-check_presence_of_vegvault <- function(relative_path = "Data/Input") {
+check_presence_of_vegvault <- function(relative_path = "Data/Input/Vegvault.sqlite") {
   vegvault_present <-
     file.exists(
-      here::here(
-        relative_path, "VegVault.sqlite"
-      )
+      here::here(relative_path)
     )
 
   if (
@@ -11,8 +9,9 @@ check_presence_of_vegvault <- function(relative_path = "Data/Input") {
   ) {
     stop(
       paste(
-        "The VegVault.sqlite file is not present in the Data/Input/ directory.",
-        "Please read the Data/Input/README.md file for more information."
+        "The VegVault.sqlite file is not present in",
+        "the `Data/Input/` directory.",
+        "Please read the `Data/Input/README.md` file for more information."
       )
     )
 
