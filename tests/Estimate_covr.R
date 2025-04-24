cd93ac3da56e57f0ec44c91a2f99953c46446ff9
Ondřej Mottl <ondrej.mottl@gmail.com>
2025-04-24 16:36:13 +0200

    Tests: make custom script to test project and make coverage report

---
+++ tests/Estimate_covr.R
@@ -0,0 +1,61 @@
+#----------------------------------------------------------#
+#
+#
+#                 Vegetation Co-occurence
+#
+#                       Run tests
+#
+#
+#                       O. Mottl
+#                         2025
+# ;
+#----------------------------------------------------------#
+# Run all tests in the project
+
+library(here)
+
+source(
+  here::here("R/___setup_project___.R")
+)
+
+library(covr)
+library(jsonlite)
+
+data_covr <-
+  covr::file_coverage(
+    source_files = list.files(
+      here::here("R/Functions/"),
+      recursive = TRUE,
+      full.names = TRUE
+    ),
+    test_files = list.files(
+      here::here("tests/testthat"),
+      recursive = TRUE,
+      full.names = TRUE
+    )
+  )
+
+data_covr %>%
+  as.data.frame() %>%
+  jsonlite::write_json(
+    path = here::here("tests/coverage.json"),
+    auto_unbox = TRUE,
+    pretty = TRUE
+  )
+
+covr::report(
+  x = covr::file_coverage(
+    source_files = list.files(
+      here::here("R/Functions/"),
+      recursive = TRUE,
+      full.names = TRUE
+    ),
+    test_files = list.files(
+      here::here("tests/testthat"),
+      recursive = TRUE,
+      full.names = TRUE
+    )
+  ),
+  file = here::here("tests/coverage.html"),
+  browse = FALSE
+)
---
+++ tests/Run_tests.R
@@ -0,0 +1,27 @@
+#----------------------------------------------------------#
+#
+#
+#                 Vegetation Co-occurence
+#
+#                       Run tests
+#
+#
+#                       O. Mottl
+#                         2025
+# ;
+#----------------------------------------------------------#
+# Run all tests in the project
+
+library(here)
+
+source(
+  here::here("R/___setup_project___.R")
+)
+
+library(testthat)
+
+testthat::test_dir(
+  here::here(
+    "tests/testthat"
+  )
+)
