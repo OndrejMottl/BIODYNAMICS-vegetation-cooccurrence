19d109896b4f7a938bb1f1be24fe25040c05a20d
Ondřej Mottl <ondrej.mottl@gmail.com>
2025-05-02 11:47:46 +0200

    01_Run_pipeline: wrap running the pipe in `try` to process to making the visualization

--- R/02_Main_analyses/01_Run_pipeline.R
+++ R/02_Main_analyses/01_Run_pipeline.R
@@ -43,9 +43,12 @@ if (
 # 2. Run the pipeline -----
 #----------------------------------------------------------#
 
-targets::tar_make(
-  script = here::here("R/02_Main_analyses/pipeline.R"),
-  store = get_active_config("target_store")
+try(
+  targets::tar_make(
+    script = here::here("R/02_Main_analyses/pipeline.R"),
+    store = get_active_config("target_store")
+  ),
+  silent = FALSE
 )
 
 #----------------------------------------------------------#
