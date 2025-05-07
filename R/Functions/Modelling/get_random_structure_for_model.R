1e47a2c87eecc15ac3f37191e49df057b2d64196
Ondřej Mottl <ondrej.mottl@gmail.com>
2025-05-07 14:49:27 +0200

    get_random_structure_for_model: add option to select how random structuree shloud be constructed

--- R/Functions/Modelling/get_random_structure_for_model.R
+++ R/Functions/Modelling/get_random_structure_for_model.R
@@ -1,66 +1,95 @@
-get_random_structure_for_model <- function(data = NULL, min_knots_distance = NULL) {
+get_random_structure_for_model <- function(
+    data = NULL,
+    type = c("age", "space"),
+    min_knots_distance = NULL) {
   `%>%` <- magrittr::`%>%`
+
+  list_random <- list()
 
   data_study_design <-
     data %>%
     purrr::chuck("data_community_to_fit") %>%
-    tibble::rownames_to_column(var = "row_names") %>%
+    add_dataset_name_column_from_rownames() %>%
     dplyr::mutate(
-      age = get_age_from_string(row_names),
-      dataset_name = get_dataset_name_from_string(row_names),
-      age_factor = factor(
-        x = age,
-        levels = sort(unique(as.numeric(age))),
-        ordered = TRUE
-      ),
       dataset_name_factor = as.factor(dataset_name)
     ) %>%
-    tibble::column_to_rownames(var = "row_names") %>%
     dplyr::select(
-      "dataset_name" = "dataset_name_factor",
-      "age" = "age_factor"
+      "dataset_name" = "dataset_name_factor"
     )
 
-  data_age <-
-    data %>%
-    purrr::chuck("data_ages_to_fit") %>%
-    dplyr::mutate(
-      age = as.numeric(age),
-    )
+  if (
+    "age" %in% type
+  ) {
+    data_study_design <-
+      data %>%
+      purrr::chuck("data_community_to_fit") %>%
+      add_age_column_from_rownames() %>%
+      add_dataset_name_column_from_rownames() %>%
+      dplyr::mutate(
+        age_factor = factor(
+          x = age,
+          levels = sort(unique(as.numeric(age))),
+          ordered = TRUE
+        ),
+        dataset_name_factor = as.factor(dataset_name)
+      ) %>%
+      dplyr::select(
+        "dataset_name" = "dataset_name_factor",
+        "age" = "age_factor"
+      )
 
-  vec_age <-
-    data_age %>%
-    dplyr::distinct(age) %>%
-    dplyr::pull(age) %>%
-    as.character()
+    data_age <-
+      data %>%
+      purrr::chuck("data_ages_to_fit") %>%
+      dplyr::mutate(
+        age = as.numeric(age),
+      )
 
-  random_level_age <-
-    Hmsc::HmscRandomLevel(
-      sData = data_age
-    )
+    vec_age <-
+      data_age %>%
+      dplyr::distinct(age) %>%
+      dplyr::pull(age) %>%
+      as.character()
 
-  data_coords <-
-    data %>%
-    purrr::chuck("data_coords_to_fit")
+    random_level_age <-
+      Hmsc::HmscRandomLevel(
+        sData = data_age
+      )
 
-  random_coors_knots <-
-    Hmsc::constructKnots(
-      sData = data_coords,
-      minKnotDist = min_knots_distance
-    )
+    list_random <-
+      c(
+        list_random,
+        list("age" = random_level_age)
+      )
+  }
 
-  random_level_coords <-
-    Hmsc::HmscRandomLevel(
-      sData = data_coords,
-      sMethod = "GPP",
-      sKnot = random_coors_knots
-    )
 
-  list_random <-
-    list(
-      "age" = random_level_age,
-      "dataset_name" = random_level_coords
-    )
+  if (
+    "space" %in% type
+  ) {
+    data_coords <-
+      data %>%
+      purrr::chuck("data_coords_to_fit")
+
+    random_coors_knots <-
+      Hmsc::constructKnots(
+        sData = data_coords,
+        minKnotDist = min_knots_distance
+      )
+
+    random_level_coords <-
+      Hmsc::HmscRandomLevel(
+        sData = data_coords,
+        sMethod = "GPP",
+        sKnot = random_coors_knots
+      )
+
+    list_random <-
+      c(
+        list_random,
+        list("dataset_name" = random_level_coords)
+      )
+  }
 
   list(
     random_levels = list_random,
