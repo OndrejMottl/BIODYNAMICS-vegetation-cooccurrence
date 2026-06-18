0366c5f02b3ea3065dfca57ba377a28b58a6cdf2
OndrejMottl <ondrej.mottl@gmail.com>
2026-06-18 22:45:09 +0200

    Pipeline architecture: add spatial and temporal visualizations with Mermaid (but do not actually include it)

---
+++ Documentation/Presentations/IAVS_2026/R/Visualisation/pipeline_mermaid_temporal.R
@@ -0,0 +1,75 @@
+#----------------------------------------------------------#
+#
+#
+#       BIODYNAMICS Vegetation Co-occurrence
+#
+#       Generate mermaid diagram for the temporal pipeline
+#       (Europe, paleo_temporal)
+#
+#----------------------------------------------------------#
+
+library(here)
+
+base::source(
+  here::here("R", "___setup_project___.R")
+)
+
+
+#----------------------------------------------------------#
+# 0. Paths -----
+#----------------------------------------------------------#
+
+path_store <-
+  here::here(
+    "Data",
+    "targets",
+    "paleo_temporal_europe",
+    "pipeline_paleo_temporal"
+  )
+
+path_script <-
+  here::here(
+    "R",
+    "Pipelines",
+    "pipeline_paleo_temporal.R"
+  )
+
+path_output <-
+  here::here(
+    "Documentation",
+    "Presentations",
+    "IAVS_2026",
+    "figures",
+    "results",
+    "slide_extra_pipeline_temporal.mmd"
+  )
+
+
+#----------------------------------------------------------#
+# 1. Generate and save -----
+#----------------------------------------------------------#
+
+# R_CONFIG_ACTIVE must be forwarded to the callr subprocess that
+# tar_mermaid() uses to source the pipeline script.
+mermaid_code <-
+  targets::tar_mermaid(
+    script = path_script,
+    store = path_store,
+    targets_only = TRUE,
+    outdated = FALSE,
+    callr_arguments = list(
+      env = c(
+        callr::rcmd_safe_env(),
+        R_CONFIG_ACTIVE = "project_paleo_temporal_europe"
+      )
+    )
+  )
+
+base::writeLines(
+  text = mermaid_code,
+  con = path_output
+)
+
+cli::cli_inform(
+  "Temporal pipeline mermaid saved to {.path {path_output}}"
+)
